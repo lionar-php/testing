8af1877cbed20a9dd57e9d2470515359a5143b11
cleanworks <cleanworks@email.com>
2016-03-16 14:17:23 +0100

    modified composer.json

--- source/TestCase.php
+++ source/TestCase.php
@@ -3,13 +3,13 @@
 namespace Lionar\Testing;
 	
 use Mockery,
-    PHPUnit_Framework_TestCase;
+PHPUnit_Framework_TestCase;
 
 class TestCase extends PHPUnit_Framework_TestCase
 {
-    public function tearDown()
-    {
-        parent::tearDown();
-        Mockery::close();
-    }
+    	public function tearDown()
+    	{
+        		parent::tearDown();
+        		Mockery::close();
+    	}
 }
